079f1edb46d5e429f7fc4717ffadb950c8565d15
Zakhar25 <132139945+Zakhar25@users.noreply.github.com>
2023-04-29 15:21:05 +0300

    Add files via upload

--- pingpong.py
+++ pingpong.py
@@ -21,14 +21,14 @@ class Player(GameSprite):
         keys_pressed = key.get_pressed()
         if keys_pressed[K_w] and self.rect.y > 5:
             self.rect.y -= self.speed
-        if keys_pressed[K_s] and self.rect.y < win_width - 80:
+        if keys_pressed[K_s] and self.rect.y < win_height - 150:
             self.rect.y += self.speed
     
     def update_right(self):
         keys_pressed = key.get_pressed()
         if keys_pressed[K_UP] and self.rect.y > 5:
             self.rect.y -= self.speed
-        if keys_pressed[K_DOWN] and self.rect.y < win_width - 80:
+        if keys_pressed[K_DOWN] and self.rect.y < win_height - 150:
             self.rect.y += self.speed
 
 background = (200, 255, 255)
@@ -45,8 +45,8 @@ FPS = 60
 
 font.init()
 font1 = font.SysFont('Arial', 36)
-win1 = font1.render('Player 1 win', 1, (255, 255, 255))
-win2 = font1.render('Player 2 win', 1, (180, 0, 0))
+win1 = font1.render('Player 1 win', 1, (0, 0, 255))
+win2 = font1.render('Player 2 win', 1, (255, 0, 0))
 
 racket1 = Player('racket.png', 30, 200, 4, 50, 150)
 racket2 = Player('racket.png', 520, 200, 4, 50, 150)
@@ -67,6 +67,22 @@ while game:
         racket2.update_right()
         ball.rect.x += speed_x
         ball.rect.y += speed_y
+
+        if sprite.collide_rect(racket1, ball) or sprite.collide_rect(racket2, ball):
+            speed_x = -speed_x
+            
+        if ball.rect.y > win_height-50 or ball.rect.y <0:
+            speed_y = -speed_y
+
+        if ball.rect.x < 0:
+            finish = True
+            window.blit(win2, (200, 200))
+
+        if ball.rect.x > win_width:
+            finish = True
+            window.blit(win2, (200, 200))
+
+            
 
         racket1.reset()
         racket2.reset()
